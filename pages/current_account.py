a2f9c9c8caa5ea613b20b7d259c972ecd7b6836a
USER <imonitieosaremen85@gmail.com>
2025-06-16 19:50:10 +0100

    Destiny

---
+++ pages/current_account.py
@@ -0,0 +1,24 @@
+
+import streamlit as st
+from current_account import CurrentAccount
+
+st.set_page_config(page_title="CurrentAccount", layout="centered")
+
+account = CurrentAccount(100000000)
+
+st.title("Current Account")
+
+with st.form("current_form"):
+    amount = st.number_input("Enter Amount",min_value=0)
+    operation = st.selectbox("Choose Operation", ["Deposit", "Withdraw"])
+    submit = st.form_submit_button("Submit")
+
+if submit:
+    if operation == "Deposit":
+        success = account.deposit(amount)
+    else:
+        success = account.withdraw(amount)  
+    if success:
+        st.success(f" Transaction successful. New Balance: \₦{account.balance:.2f}")
+    else:
+        st.error("Transaction failed. Check amount or overdraft limit.")
