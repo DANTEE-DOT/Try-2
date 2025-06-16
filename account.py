10605e7355353756fc4f7b8a0129e7fb946b607e
ctrltw <danjumaatoika@gmail.com>
2025-06-16 18:36:06 +0100

    Victor

---
+++ account.py
@@ -0,0 +1,15 @@
+class Account:
+    def __init__(self, balance=0):
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            return True
+        return False
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            return True
+        return False
