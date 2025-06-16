8b7e2a4c2d8fe51591082fe41d9860f7e58a0c0b
DANTEE-DOT <tsidkenu-temisan.daniel@binghamuni.edu.ng>
2025-06-16 18:24:29 +0100

    Dantee 2.0

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Twisco Bank", layout="centered")
+
+st.title(" Welcome to Trust bank")
+st.markdown("""
+TrustBank offers safe, fast, and easy banking at your fingertips.
+Choose an option below to begin.
+""")
+st.markdown("- Savings Account")
+st.markdown("- Current Account")
