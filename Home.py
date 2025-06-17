e08fe91b00ac78eb91d326fc034fdba48d7b3637
DANTEE-DOT <tsidkenu-temisan.daniel@binghamuni.edu.ng>
2025-06-17 18:45:33 +0100

    Naming

--- Home.py
+++ Home.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 st.set_page_config(page_title="Twisco Bank", layout="centered")
 
-st.title(" Welcome to Trust bank")
+st.title(" Welcome to Twisco Bank")
 st.markdown("""
-TrustBank offers safe, fast, and easy banking at your fingertips.
+Twisco Bank offers safe, fast, and easy banking at your fingertips.
 Choose an option below to begin.
 """)
 st.markdown("- Savings Account")
